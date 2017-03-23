b9cd90956dff04795b05a3996b54ac26954d5eb7
Stepan Snigirev <stepan.snigirev@mpq.mpg.de>
2017-03-23 15:48:13 +0100

    demo page fixed

--- demo/app.js
+++ demo/app.js
@@ -3,43 +3,34 @@ var app = new Vue({
 	data: {
 		chart: null,
 		system: {
-			q_in: 1e3, // heat that we get from the heating tape
+			q_in: 10, // heat that we get from the heating device (peltie or heating tape)
 			c_heat: 3, // heat capacitance of the heating region
 			c_det: 3, // heat capacitance of the detection region
 			t_heat: 20, // temperature of the heating region
 			t_det: 20, // temperature of the detection region
-			t_ext: 20,	// external temperature
-			kappa_int: 10, // heat conductivity within a bulk
+			kappa_int: 30, // heat conductivity within a bulk
 			kappa_ext: 3, // heat conductivity to external bath
-			dt: 1e-4, // simulating step
-			t_sample: 3e-3, // sampling rate for the data array
+			t_ext: 20,	// external temperature
+			dt: 1e-4, // simulation step
+			t_sample: 1e-3, // sampling rate for the plot
+			noize: 0,
 		},
 		pid: {
-			tset: 40,
+			tset: 200,
 
 			// mode: "relay",
 			mode: "pid",
-			kp: 5e-1,
-			ki: 5e-1,
-			kd: 2e-2,
+			kp: 30,
+			ki: 15,
+			kd: 8e-1,
 
 			// dt: 1e-6, // relay time step
-			dt: 1/60, // relay time step
-			t: 2, // simulation range up to t
-			umin: -1, // max output value
-			umax: 1, // min output value
+			dt: 1e-3, // relay time step
+			t: 1, // simulation range up to t
+			umin: -10e3, // max output value
+			umax: 10e3, // min output value
 		}
 	},
- 	// 	computed: {
-	// 	series: function () {
-	// 		return [];
-	// 	},
-	// },
-	// watch:{
-	// 	series: function(val){
-	// 		// this.redraw()
-	// 	},
-	// },
 	methods: {
 		getSeries: function(){
 			var sys = Bulk(this.system);
@@ -47,6 +38,7 @@ var app = new Vue({
 			settings.system = sys;
 			var pid = PID(settings);
 			var data = pid();
+			console.log(data);
 			return [{
 				name: "Measured temperature",
 				data: data.t_det,
@@ -58,7 +50,10 @@ var app = new Vue({
 			}];
 		},
 		redraw: function(){
-			this.draw();
+			// this.draw();
+			var ser = this.getSeries();
+			this.chart.series[0].setData(ser[0].data);
+			this.chart.series[1].setData(ser[1].data);
 		},
 		draw: function(){
 			this.chart = plot("chart", this.getSeries());
