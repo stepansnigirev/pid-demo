22e597b691d9f197e80527a63f45710fa867acde
Stepan Snigirev <stepan.snigirev@mpq.mpg.de>
2017-03-23 16:13:34 +0100

    index page

--- demo/app.js
+++ demo/app.js
@@ -3,16 +3,16 @@ var app = new Vue({
 	data: {
 		chart: null,
 		system: {
-			q_in: 10, // heat that we get from the heating device (peltie or heating tape)
-			c_heat: 3, // heat capacitance of the heating region
-			c_det: 3, // heat capacitance of the detection region
+			q_in: 100, // heat that we get from the heating device (peltie or heating tape)
+			c_heat: 30, // heat capacitance of the heating region
+			c_det: 30, // heat capacitance of the detection region
 			t_heat: 20, // temperature of the heating region
 			t_det: 20, // temperature of the detection region
 			kappa_int: 30, // heat conductivity within a bulk
 			kappa_ext: 3, // heat conductivity to external bath
 			t_ext: 20,	// external temperature
-			dt: 1e-4, // simulation step
-			t_sample: 1e-3, // sampling rate for the plot
+			dt: 1e-3, // simulation step
+			t_sample: 1e-2, // sampling rate for the plot
 			noize: 0,
 		},
 		pid: {
@@ -20,15 +20,15 @@ var app = new Vue({
 
 			// mode: "relay",
 			mode: "pid",
-			kp: 30,
-			ki: 15,
-			kd: 8e-1,
+			kp: 54,
+			ki: 160,
+			kd: 0.24,
 
 			// dt: 1e-6, // relay time step
 			dt: 1e-3, // relay time step
-			t: 1, // simulation range up to t
-			umin: -10e3, // max output value
-			umax: 10e3, // min output value
+			t: 10, // simulation range up to t
+			umin: -100, // max output value
+			umax: 100, // min output value
 		}
 	},
 	methods: {
